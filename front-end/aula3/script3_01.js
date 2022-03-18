80b78947a8b9022fdda4f31f4bc5097414540d90
Gabriel <gonzagagabriel459@gmail.com>
2022-03-17 21:18:12 -0300

    modificações

--- front-end/aula3/script3_01.js
+++ front-end/aula3/script3_01.js
@@ -1,6 +1,22 @@
-var dia, nota;
-dia = 25;
-nota = 9.7;
-document.write (dia);
-document.write("<br>");
-document.write (nota);
+var dia, nota, ano, mes, nome;
+var usuario=prompt("Digite o usuario");
+document.write("Usuario: "+usuario)
+document.write("<hr>")
+dia=15;
+nota=9.8; // anotação de número quebrado tem que ser com ponto, não virgula
+ano=2022;
+mes="março";
+nome="Gabriel";
+document.write(dia);//15
+document.write("<hr>");
+document.write(nota);//9.8
+document.write("<hr>");
+document.write(dia + "/" + mes + "/" + ano);
+document.write("<hr>");
+document.write("Olá, meu nome é " + nome + ". Eu tirei nota: " + nota + ".");
+document.write("Usuario"+usuario);
+/*
+Saída
+15/março/2022
+Hebert eu tirei nota 9.8
+*/
