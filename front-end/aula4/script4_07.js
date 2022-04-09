c80e78659620a9fbee7ff3ae82ce45518eb1f54e
Gabriel <gonzagagabriel459@gmail.com>
2022-04-08 21:01:39 -0300

    terminando

--- front-end/aula4/script4_07.js
+++ front-end/aula4/script4_07.js
@@ -2,11 +2,13 @@
 var a, b, res;
 a = 10; // 10   int
 b = "10"; // 10 string
-res=(a==b); // res = true
+res=(a===b); // res = true
 document.write("res = " + res);
 
 /*
 =       , recebe ou atribui
 ==      , igualdade, checa apenas o valor, não o tipo
-===     , 
+===     , identicos, verifica o valor e o tipo
+!==     , não igual, diferentes 
+!==     , não identico
 */
