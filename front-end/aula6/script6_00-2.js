57ff660638ae40edce5887854f6cc5b2ecdd3de5
Gabriel <gonzagagabriel459@gmail.com>
2022-05-30 13:50:06 -0300

    Atividades novas

---
+++ front-end/aula6/script6_00-2.js
@@ -0,0 +1,11 @@
+var i, cidade;
+i=10;
+i=11;
+i=12;
+i=13; 
+//         0            1           2           3 
+cidade=["São Paulo","Pedreira","São Pedro","guarulhos"];
+document.querySelector("#dv1").innerHTML=cidade;
+document.querySelector(".teste").innerHTML=cidade[0];
+document.querySelector("h2").innerHTML=cidade[3];
+
